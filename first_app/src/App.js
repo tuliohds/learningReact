34a37754e8577cdd991056d1219dadd8ca82c6bb
Tulio Henrique <tuliohenriques@hotmail.com>
2023-05-09 17:05:46 -0300

    feat: create a counter using component and state

--- first_app/src/App.js
+++ first_app/src/App.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App(){
-  return(
-    <div>
-      <h1>React</h1>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      name: 'Tulio',
+      counter: 0
+    };
+
+    this.increase = this.increase.bind(this);
+    this.decrease = this.decrease.bind(this);
+  }
+
+  increase(){
+    let state = this.state;
+    state.counter += 1;
+    this.setState(state);
+  }
+
+  decrease(){
+    let state = this.state;
+    if(state.counter === 0){
+      alert('is not possible decrease zero');
+      return;
+    }
+    state.counter -= 1;
+    this.setState(state);
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Counter by {this.state.name}</h1>
+        <h3>
+          <div>
+            <button onClick={this.decrease}> - </button>
+              {this.state.counter} 
+            <button onClick={this.increase}> + </button>
+          </div>
+        </h3>
+      </div>
+    );
+  }
 }
 
 export default App;
