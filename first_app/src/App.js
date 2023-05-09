bf1795b88b6c7a405c1ba12938c0ffc6539f2d9a
Tulio Henrique <tuliohenriques@hotmail.com>
2023-05-09 17:23:55 -0300

    change: isolate counter

--- first_app/src/App.js
+++ first_app/src/App.js
@@ -2,44 +2,10 @@ import React, { Component } from 'react';
 
 class App extends Component {
 
-  constructor(props){
-    super(props);
-    this.state = {
-      name: 'Tulio',
-      counter: 0
-    };
-
-    this.increase = this.increase.bind(this);
-    this.decrease = this.decrease.bind(this);
-  }
-
-  increase(){
-    let state = this.state;
-    state.counter += 1;
-    this.setState(state);
-  }
-
-  decrease(){
-    let state = this.state;
-    if(state.counter === 0){
-      alert('it is not possible to decrease zero');
-      return;
-    }
-    state.counter -= 1;
-    this.setState(state);
-  }
-
   render(){
     return(
       <div>
-        <h1>Counter by {this.state.name}</h1>
-        <h3>
-          <div>
-            <button onClick={this.decrease}> - </button>
-              {this.state.counter} 
-            <button onClick={this.increase}> + </button>
-          </div>
-        </h3>
+        <h1>My project</h1>
       </div>
     );
   }
---
+++ first_app/src/Counter.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      name: 'Tulio',
+      counter: 0
+    };
+
+    this.increase = this.increase.bind(this);
+    this.decrease = this.decrease.bind(this);
+  }
+
+  increase(){
+    let state = this.state;
+    state.counter += 1;
+    this.setState(state);
+  }
+
+  decrease(){
+    let state = this.state;
+    if(state.counter === 0){
+      alert('it is not possible to decrease zero');
+      return;
+    }
+    state.counter -= 1;
+    this.setState(state);
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Counter by {this.state.name}</h1>
+        <h3>
+          <div>
+            <button onClick={this.decrease}> - </button>
+              {this.state.counter} 
+            <button onClick={this.increase}> + </button>
+          </div>
+        </h3>
+      </div>
+    );
+  }
+}
+
+export default Counter;
