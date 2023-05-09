cb1c7a6dec874e018f2b5942f893791a386a11cf
Tulio Henrique <tuliohenriques@hotmail.com>
2023-05-09 17:47:23 -0300

    feat: create timer learning component lifecyle

---
+++ first_app/src/Timer.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      hour: '00:00:00'
+    };
+  }
+
+  componentDidMount() {
+    // when the app is initialized this component will be executed
+    setInterval(() => {this.setState({
+      hour: new Date().toLocaleTimeString()
+    })}, 1000);
+  }
+
+  componentDidUpdate() {
+    // when the app is reload this component will be executed
+    console.log('updating...');
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Timer</h1>
+        <p>Practicing about component lifecycle</p>
+        <h1>{this.state.hour}</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
