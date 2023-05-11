40923f3feb04bbdc85486852f5262b3a07959841
Tulio Henrique <tuliohenriques@hotmail.com>
2023-05-11 16:31:22 -0300

    feat: using conditional rendering

--- first_app/src/App.js
+++ first_app/src/App.js
@@ -1,16 +1,55 @@
 import React, { Component } from 'react';
-import Counter from './componentLifeCycle/Counter';
-import Timer from './Timer';
+import Counter from './components/lifeCycle/Counter';
+import Timer from './components/Timer';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      counterActivated: false,
+      timerActivated: false,
+    };
+
+    this.activeCounter = this.activeCounter.bind(this);
+    this.activeTimer = this.activeTimer.bind(this);
+    this.desativeAll = this.desativeAll.bind(this);
+  }
+
+  activeCounter(){
+    let state = this.state;
+    state.counterActivated = true;
+    this.setState(state);
+  }
+
+  activeTimer(){
+    let state = this.state;
+    state.timerActivated = true;
+    this.setState(state);
+  }
+
+  desativeAll(){
+    let state = this.state;
+    state.counterActivated = false;
+    state.timerActivated = false;
+    this.setState(state);
+  }
 
   render(){
     return(
       <div>
         <h1>My projects on React</h1>
+        <p>Click on buttons below for see the developments</p>
         <div>
-          <Counter />
-          <Timer />
+          <button onClick={this.activeCounter}> COUNTER </button>
+          <button onClick={this.activeTimer}> TIMER </button>
+          <button onClick={this.desativeAll}> DESATIVE ALL </button>
+          {
+            this.state.counterActivated === true && <Counter />
+          }
+          {
+            this.state.timerActivated === true && <Timer />
+          }
         </div>
       </div>
     );
